96e2a1c92b3ed729e2ee208c2b76d5117d8c1a1f
tarao <tarao.gnn@gmail.com>
2022-03-04 10:54:24 +0900

    Rewrite tests using summon.

--- src/test/scala/lambda/EvalSpec.scala
+++ src/test/scala/lambda/EvalSpec.scala
@@ -1,20 +1,19 @@
 package lambda
 
 object Eval1Spec {
-  def eval1: Eval.Eval1[DeBruijn.Of[App[Abs["x", Abs["y", Var["x"]]], Abs["y", Var["y"]]]]] =
-    (DeBruijn.Abs("y", DeBruijn.Abs("y", DeBruijn.Var(1, "y"))), true)
+  summon[Eval.Eval1[DeBruijn.Of[App[Abs["x", Abs["y", Var["x"]]], Abs["y", Var["y"]]]]] =:=
+    (DeBruijn.Abs["y", DeBruijn.Abs["y", DeBruijn.Var[1, "y"]]], true)]
 
-  def eval2: Eval.Eval1[DeBruijn.Of[App[Abs["x", App[Var["x"], Abs["y", Var["x"]]]], Abs["y", Var["y"]]]]] =
-    (DeBruijn.App(DeBruijn.Abs("y", DeBruijn.Var(1, "y")), DeBruijn.Abs("y", DeBruijn.Abs("y", DeBruijn.Var(1, "y")))), true)
+  summon[Eval.Eval1[DeBruijn.Of[App[Abs["x", App[Var["x"], Abs["y", Var["x"]]]], Abs["y", Var["y"]]]]] =:=
+    (DeBruijn.App[DeBruijn.Abs["y", DeBruijn.Var[1, "y"]], DeBruijn.Abs["y", DeBruijn.Abs["y", DeBruijn.Var[1, "y"]]]], true)]
 
-  def eval3: Eval.Eval1[DeBruijn.Of[Abs["y", App[Abs["x", Abs["y", Var["x"]]], Abs["z", Var["y"]]]]]] =
-    (DeBruijn.Abs("y", DeBruijn.Abs("y", DeBruijn.Abs("z", DeBruijn.Var(3, "y")))), true)
+  summon[Eval.Eval1[DeBruijn.Of[Abs["y", App[Abs["x", Abs["y", Var["x"]]], Abs["z", Var["y"]]]]]] =:=
+    (DeBruijn.Abs["y", DeBruijn.Abs["y", DeBruijn.Abs["z", DeBruijn.Var[3, "y"]]]], true)]
 
-  def eval4: Eval.Eval1[DeBruijn.Of[Abs["z", App[Abs["x", App[Var["z"], Abs["y", Var["x"]]]], Abs["y", Var["y"]]]]]] =
-    (DeBruijn.Abs("z", DeBruijn.App(DeBruijn.Var(1, "z"), DeBruijn.Abs("y", DeBruijn.Abs("y", DeBruijn.Var(1, "y"))))), true)
+  summon[Eval.Eval1[DeBruijn.Of[Abs["z", App[Abs["x", App[Var["z"], Abs["y", Var["x"]]]], Abs["y", Var["y"]]]]]] =:=
+    (DeBruijn.Abs["z", DeBruijn.App[DeBruijn.Var[1, "z"], DeBruijn.Abs["y", DeBruijn.Abs["y", DeBruijn.Var[1, "y"]]]]], true)]
 }
 
 object EvalSpec {
-  def eval1: Show[Eval[App[App[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]], Abs["x", Abs["y", Var["x"]]]], Abs["x", Abs["y", Var["x"]]]]]] =
-    "λz.z"
+  summon[Show[Eval[App[App[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]], Abs["x", Abs["y", Var["x"]]]], Abs["x", Abs["y", Var["x"]]]]]] =:= "λz.z"]
 }
--- src/test/scala/lambda/TermSpec.scala
+++ src/test/scala/lambda/TermSpec.scala
@@ -1,24 +1,17 @@
 package lambda
 
 object TermSpec {
-  def show1: Show[Var["a"]] =
-    "a"
+  summon[Show[Var["a"]] =:= "a"]
 
-  def show2: Show[Abs["a", Var["a"]]] =
-    "λa.a"
+  summon[Show[Abs["a", Var["a"]]] =:= "λa.a"]
 
-  def show3: Show[App[Abs["a", Var["a"]], Var["b"]]] =
-    "(λa.a) b"
+  summon[Show[App[Abs["a", Var["a"]], Var["b"]]] =:= "(λa.a) b"]
 
-  def show4: Show[Abs["x", Abs["y", Var["x"]]]] =
-    "λxy.x"
+  summon[Show[Abs["x", Abs["y", Var["x"]]]] =:= "λxy.x"]
 
-  def show5: Show[Abs["x", Abs["y", Var["x"]]]] =
-    "λxy.x"
+  summon[Show[Abs["x", Abs["y", Var["x"]]]] =:= "λxy.x"]
 
-  def show6: Show[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]]] =
-    "λxyz.x z (y z)"
+  summon[Show[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]]] =:= "λxyz.x z (y z)"]
 
-  def show7: Show[App[App[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]], Abs["x", Abs["y", Var["x"]]]], Abs["x", Abs["y", Var["x"]]]]] =
-    "(λxyz.x z (y z)) (λxy.x) (λxy.x)"
+  summon[Show[App[App[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]], Abs["x", Abs["y", Var["x"]]]], Abs["x", Abs["y", Var["x"]]]]] =:= "(λxyz.x z (y z)) (λxy.x) (λxy.x)"]
 }
