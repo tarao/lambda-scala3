83bd6a0b85692e023be5ae4c14dfb11dc9f85221
tarao <tarao.gnn@gmail.com>
2022-03-04 15:53:45 +0900

    Some more tests.

--- src/test/scala/lambda/EvalSpec.scala
+++ src/test/scala/lambda/EvalSpec.scala
@@ -1,5 +1,7 @@
 package lambda
 
+import scala.compiletime.ops.int
+
 object Eval1Spec {
   summon[Eval.Eval1[DeBruijn.Of[App[Abs["x", Abs["y", Var["x"]]], Abs["y", Var["y"]]]]] =:=
     (DeBruijn.Abs["y", DeBruijn.Abs["y", DeBruijn.Var[1, "y"]]], true)]
@@ -15,5 +17,74 @@ object Eval1Spec {
 }
 
 object EvalSpec {
-  summon[Show[Eval[App[App[Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]], Abs["x", Abs["y", Var["x"]]]], Abs["x", Abs["y", Var["x"]]]]]] =:= "λz.z"]
+  type s = Abs["x", Abs["y", Abs["z", App[App[Var["x"], Var["z"]], App[Var["y"], Var["z"]]]]]]
+  type k = Abs["x", Abs["y", Var["x"]]]
+  type ks = App[k, s]
+  type skk = App[App[s, k], k]
+  type skkski = App[App[s, App[App[s, ks], k]], skk]
+
+  type yy = Abs["x", App[Var["f"], App[Var["x"], Var["x"]]]]
+  type y = Abs["f", App[yy, yy]]
+  type zz = Abs["x", App[Var["f"], Abs["y", App[App[Var["x"], Var["x"]], Var["y"]]]]]
+  type z = Abs["f", App[zz, zz]]
+
+  summon[Show[Eval[App[App[s, k], k]]] =:= "λz.z"]
+
+  summon[Show[Eval[App[App[s, k], k]]] =:= "λz.z"]
+
+  summon[Show[Eval[skkski]] =:= "λzz.z (z z)"]
+
+  summon[Show[Eval[App[App[skkski, Var["a"]], Var["b"]]]] =:= "a (a b)"]
+
+  type S[n <: Int] <: Term = n match {
+    case 0 => Var["z"]
+    case _ => App[Var["s"], S[int.+[n, -1]]]
+  }
+
+  type Church[n <: Int] <: Term = n match { case _ => Abs["s", Abs["z", S[n]]] }
+
+  type _0 = Church[0]
+  type _1 = Church[1]
+  type _2 = Church[2]
+  type _3 = Church[3]
+  type _4 = Church[4]
+  type _5 = Church[5]
+  type _6 = Church[6]
+
+  type t = k
+  type f = Abs["x", Abs["y", Var["y"]]]
+  type if0 = Abs["n", App[App[Var["n"], Abs["x", f]], t]]
+  type mul = Abs["m", Abs["n", Abs["s", Abs["z", App[
+    App[Var["n"], App[Var["m"], Var["s"]]],
+    Var["z"]
+  ]]]]]
+  type pred = Abs["n", Abs["s", Abs["z", App[
+    App[
+      App[Var["n"], Abs["f", Abs["g", App[Var["g"], App[Var["f"], Var["s"]]]]]],
+      Abs["x", Var["z"]]
+    ],
+    Abs["x", Var["x"]]
+  ]]]]
+  type fact = Abs["r", Abs["n",
+    App[
+      App[
+        App[if0, Var["n"]],                // if 0 = n
+        _1                                 // then 1
+      ],
+      App[                                 // otherwise
+        App[mul, Var["n"]],                // n *
+        App[Var["r"], App[pred, Var["n"]]] // fact(n-1)
+      ]
+    ]
+  ]]
+
+  summon[Show[Eval[App[App[App[if0, _0], Var["a"]], Var["b"]]]] =:= "a"]
+  summon[Show[Eval[App[App[App[if0, _1], Var["a"]], Var["b"]]]] =:= "b"]
+  summon[Show[Eval[App[App[mul, _2], _2]]] =:= Show[_4]]
+  summon[Show[Eval[App[App[mul, _2], _3]]] =:= Show[_6]]
+  summon[Show[Eval[App[App[y, fact], _3]]] =:= Show[_6]] // this takes long
+
+  type o = Abs["a", App[Var["a"], Var["a"]]]
+
+  summon[Show[Eval[App[App[k, Var["c"]], App[o, o]]]] =:= "c"] // the strategy is normalizing
 }
